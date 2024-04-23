615d07e98604d59967cffc99fd5744adba1d2c07
Latex <YoKytra@gmail.com>
2024-04-23 13:45:43 +0300

    asda

--- Python/e approximator.py
+++ Python/e approximator.py
@@ -1,16 +1,17 @@
-def factorial(n):
-    n1 = n;
+def factorial(n): # function that calculates the factorial of any integer n
+    n1 = int(n);
     if(n == 0):
         return 1
     for i in range(1, n):
         n1 = n1 * (i)
     return n1
 
-def approxE(nOfApprox):
+def approxE(nOfApprox): # Approximate e using the 1/n! sum
     e = 0
     for i in range(0, nOfApprox):
         e += 1/(factorial(i))
     return e
+
 print("Number of times to approximate e?\n")
 nOfApprox = int(input())
 print("e is approximately equal to: " + str(approxE(nOfApprox)))
