78c75364e0e1d20775da9d1f5a40b478b946515f
latex <yokytra@gmail.com>
2024-06-06 15:18:46 +0300

    committing all the webserver stuff with esp32

--- Arduino/WebServer/WebServer.ino
+++ Arduino/WebServer/WebServer.ino
@@ -7,13 +7,13 @@
 const char* SSID = "Alex-home";
 const char* PASS = "AlexIana2005";
 
-String webpage = "<!DOCTYPE html><html> <head> <title>fucking bitch site</title> <style> body{ background-color: #212121; color:gold; } </style> </head> <body> <h1>Generating random number</h1> <p>The numbers are: <span id='rand1'>-</span></p> <p><span id='rand2'>-</span></p> <button id='BUT' onclick='BUT_SEND()'>Send info</button> </body> <script> var Socket; function init(){ Socket = new WebSocket('ws://' + window.location.hostname + ':81/'); Socket.onmessage = function(event){ processCommand(event); }; } function BUT_SEND(){ var msg = { brand: 'Harley Benton', type: 'TE-20HH', year: 'idk', color: 'black' }; Socket.send(JSON.stringify(msg)); } function processCommand(event){ var obj = JSON.parse(event.data); document.getElementById('rand1').innerHTML = obj.rand1; document.getElementById('rand2').innerHTML = obj.rand2; console.log(obj.rand1); console.log(obj.rand2); } window.onload = function(event){ init(); } </script></html>";
-
-int interval = 1000;
-unsigned long previousMillis = 0;
+String webpage = "<!DOCTYPE html><html> <head> <title>fucking bitch site</title> <style> body{ background-color: #212121; color:gold; } </style> </head> <body> <h1>Control the LEDs</h1> <form> <input type='radio' id='LED0' name='radio' onclick='changeLED(0)'> <label for='LED0'>LED0</label> <input type='radio' id='LED1' name='radio' onclick='changeLED(1)'> <label for='LED1'>LED1</label> <input type='radio' id='LED2' name='radio' onclick='changeLED(2)'> <label for='LED2'>LED2</label> </form> <input type='range' id='slider' min='0' max='255' value='127' onclick='brightness(slider.value)'><br> <label for='slider'>Value: <span id='sliderval'>-</span></label> </body> <script> var Socket; output = document.getElementById('sliderval'); slider = document.getElementById('slider'); function init(){ Socket = new WebSocket('ws://' + window.location.hostname + ':81/'); Socket.onmessage = function(event){ processCommand(event); }; } function changeLED(ledNumber){ console.log(ledNumber); let msg = { type: 'ledNumber', value: ledNumber }; Socket.send(JSON.stringify(msg)); } function brightness(intensity){ console.log(intensity); let msg = { type: 'brightness', value: intensity }; } function processCommand(event){ var obj = JSON.parse(event.data); let type = obj.type; if(type.localeCompare('intensity') == 0){ let intensity = parseInt(obj.value); slider.value = intensity; output.value = intensity; }else if(type.localeCompare('ledNumber')){ let l_ledChanged = parseInt(obj.value); if(l_ledChanged == 0){ document.getElementById('LED0').checked = true; }else if(l_ledChanged == 1){ document.getElementById('LED1').checked = true; }else if(l_ledChanged == 2){ document.getElementById('LED2').checked = true; } } } window.onload = function(event){ init(); } </script></html>";
 
 WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
+
+int brightness;
+int ledNumber;
 
 void setup() {
   Serial.begin(115200);
@@ -37,28 +37,14 @@ void setup() {
 void loop() {
   server.handleClient();
   webSocket.loop();
-
-  unsigned long now = millis();
-  if ((unsigned long)(now - previousMillis) > interval) {
-    String JsonString = "";
-    StaticJsonDocument<200> doc;                      // create a JSON container
-    JsonObject obj = doc.to<JsonObject>();
-    obj["rand1"] = random(255);
-    obj["rand2"] = random(255);
-    serializeJson(doc, JsonString);
-    Serial.println(JsonString);
-
-    webSocket.broadcastTXT(JsonString);
-    previousMillis = now;
-  }
 }
 
 void webSocketEvent(byte num, WStype_t type, uint8_t* payload, size_t length) {
   switch (type) {
     case WStype_DISCONNECTED:  // If client disconnected, print it
-      Serial.println("Client disconnected.");
+      Serial.println("Client " + num + " disconnected.");
     case WStype_CONNECTED:  // check if websocket client is connected
-      Serial.println("Client connected.");
+      Serial.println("Client " + num + "  connected.");
       break;
     case WStype_TEXT:  // Check response from client
       StaticJsonDocument<200> doc;                    // create a JSON container
@@ -67,17 +53,31 @@ void webSocketEvent(byte num, WStype_t type, uint8_t* payload, size_t length) {
         Serial.println("Shit!");
         return;
       } else {
-        const char* brand = doc["brand"];
         const char* type = doc["type"];
-        const int year = doc["year"];
-        const char* color = doc["color"];
-        Serial.println("Received guitar info");
-        Serial.println("Brand: " + String(brand));
-        Serial.println("Type: " + String(type));
-        Serial.println("Year: " + String(year));
-        Serial.println("Color: " + String(color));
+        const int value = doc["value"];
+        Serial.println(String(type));
+        Serial.println(String(value));
+        
+        if(String(type == "intensity")){
+          brightness = value;
+          sendJSON('intensity', String(value));
+        }
+        if(String(type == "ledNumber")){
+          ledNumber = value;
+          sendJSON('ledNumber', String(value));
+        }
       }
       Serial.println("");
       break;
   }
+}
+
+void sendJSON(String type, String value){
+  String jsonString = "";
+  StaticJsonDocument<200> doc;
+  JsonObject obj = doc.to<JsonObject>();
+  obj['type'] = type;
+  obj['value'] = value;
+  serializeJson(doc, jsonString);
+  webSocket.broadcastTXT(jsonString);
 }
