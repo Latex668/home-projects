d9b4ca431decd35676931949099354acff654db1
latex <yokytra@gmail.com>
2024-07-21 22:48:45 +0300

    asd

--- webdesign/calculator site/index.js
+++ webdesign/calculator site/index.js
@@ -1,8 +1,14 @@
 let current = document.getElementById('Current').value;
 let voltage = document.getElementById('Voltage').value;
 let resistance = document.getElementById('Resistance').value;
+let RChanged = 0;
 
-function ohmsLaw(voltage, current, resistance){
+
+
+function ohmsLaw(voltage, current, resistance, RChanged, IChanged, VChanged){
+    if(RChanged>2){
+        RChanged=0;
+    }
     if(resistance == 0){
         return document.getElementById('Resistance').value = voltage/current;
     }else if(voltage == 0){
@@ -19,6 +25,29 @@ function Power(voltage, current, resistance){
         return document.getElementById('Power').value = (voltage * voltage)/resistance;
     }else{
         return document.getElementById('Power').value = current * current * resistance;
+    }
+}
+
+function changeUnit(RUnit, IUnit, VUnit){
+    if(RUnit){
+        console.log("Changed R unit");
+        console.log(RChanged);
+        switch(RChanged){
+            case 0:
+                RChanged=1;
+                document.getElementById("RUnit").innerHTML = "K ohms";
+                break;
+            case 1:
+                RChanged=2;
+                document.getElementById("RUnit").innerHTML = "M ohms";
+                break;
+            case 2:
+                RChanged=0;
+                document.getElementById("RUnit").innerHTML = "Ohms";
+                break;
+
+
+        }
     }
 }
 
